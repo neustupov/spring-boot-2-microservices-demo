da2c9f35d91e5ddf4a311b15ddc1323b5ab9b773
Dmitry_Neustupov <neustupov@yandex.ru>
2020-01-04 16:33:04 +0300

    add spring boot actuator and yaml property file

--- src/main/java/ru/neustupov/springboot2microservicesdemo/rest/UserController.java
+++ src/main/java/ru/neustupov/springboot2microservicesdemo/rest/UserController.java
@@ -1,7 +1,6 @@
 package ru.neustupov.springboot2microservicesdemo.rest;
 
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.neustupov.springboot2microservicesdemo.model.User;
@@ -10,8 +9,11 @@ import ru.neustupov.springboot2microservicesdemo.model.UserRepository;
 @RestController
 public class UserController {
 
-  @Autowired
   private UserRepository userRepository;
+
+  public UserController(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
 
   @GetMapping("/users")
   public List<User> allUsers() {
